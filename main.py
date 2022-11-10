913268d60abca7486e3d0e648cd7635d2f76a2cb
prins <quinton_kelley@yahoo.com>
2022-11-10 11:06:43 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Welcome to my survey!")
+print("Please complete survey to help us decide what to play next!")
+
+
+a = "Days Gone"
+
+b = "Resident Evil"
+
+c = "Fortnite"
+
+d = "Apex Legends"
+
+e = "Death Stranding"
+
+f = "Surprise Us!"
+
+print(f"""Here are your options, remember to only choose one: 
+
+a){a},
+b){b},
+c){c},
+d){d},
+e){e},
+f){f}""")
+
+selection = input("Please make a selection: ")
+
+print(f"You have chosen {selection}. I appreciate your help and hope to see you on the next one!")
+
+
+
